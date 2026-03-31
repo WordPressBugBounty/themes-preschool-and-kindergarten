ea3db4c218dbc399d9d2d57a0a24c862b597b44a
Sean Murphy <sean@iamseanmurphy.com>
2026-03-31 12:36:30 +0000

    1.2.7

--- preschool-and-kindergarten/build/dashboard.asset.php
+++ preschool-and-kindergarten/build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0eb53b35610f062dc89b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '45b700aa52723e6013a6');
